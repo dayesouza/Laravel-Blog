d8c05f6275bc0ecacc33042da110fbf30bda7ab5
Day Souza <daysouza@DayMac.local>
2019-02-18 22:57:09 -0300

    Initial commit

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Days Blog</title>
+
+</head>
+
+<body>
+<h1>SERVICES</h1>
+<div class="alert alert-danger">
+    AAAA
+</div>
+</body>
+
+</html>
